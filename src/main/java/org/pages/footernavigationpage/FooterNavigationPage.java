26c9670d1ab5ad694db3ce2b26639288e99accdf
Marian <lilmaryan2001@gmail.com>
2023-01-23 20:33:44 +0200

    lesson5 page object , windows navigation , singleton

---
+++ src/main/java/org/pages/footernavigationpage/FooterNavigationPage.java
@@ -0,0 +1,21 @@
+package org.pages.footernavigationpage;
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class FooterNavigationPage {
+
+    public void clickTwitterItem() {
+        $(By.xpath("//ul[@class='social']/li[@class='social_twitter']")).shouldBe(Condition.enabled).click();
+    }
+
+    public void clickFacebookItem() {
+        $(By.xpath("//ul[@class='social']/li[@class='social_facebook']")).shouldBe(Condition.enabled).click();
+    }
+
+    public void clickLinkedInItem() {
+        $(By.xpath("//ul[@class='social']/li[@class='social_linkedin']")).shouldBe(Condition.enabled).click();
+    }
+}
