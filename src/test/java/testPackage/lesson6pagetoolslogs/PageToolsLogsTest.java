7b08b81016e884f10687619d2691d081e7c0895e
VladyslavKoshman <vko@primexm.com>
2023-01-26 22:04:01 +0200

    lesson 6 logger

--- src/test/java/testPackage/lesson6pagetoolslogs/PageToolsLogsTest.java
+++ src/test/java/testPackage/lesson6pagetoolslogs/PageToolsLogsTest.java
@@ -1,5 +1,6 @@
 package testPackage.lesson6pagetoolslogs;
 
+import com.codeborne.selenide.Selenide;
 import org.base.BaseTest;
 import org.pages.Pages;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ public class PageToolsLogsTest extends BaseTest {
     public void test(){
         Pages.loginPage().typeLoginInput("standard_user");
         Pages.loginPage().typePasswordInput("secret_sauce");
-
+        Pages.loginPage().clickEnterButton();
+        Selenide.sleep(3000);
     }
 }
